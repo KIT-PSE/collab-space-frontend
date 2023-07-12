2788d4957a2f8b8b221ec9fe8af3e83c124d9503
Florian Raith <37345813+FlorianRaith@users.noreply.github.com>
2023-07-12 13:19:53 +0200

    Add hand signals (#25)
    * Add hand signal toggle
    
    * Implement hand signal (Socket)
    
    * Remove hand signal from the teacher's room view
    
    * refactor code
    
    ---------
    
    Co-authored-by: neda_moshki <uzijb@student.kit.edu>
    Co-authored-by: Marius Friess <friess.marius@gmail.com>

--- src/composables/channel.ts
+++ src/composables/channel.ts
@@ -19,6 +19,7 @@ export interface ChannelUser {
 
 export interface Student extends ChannelUser {
   name: string;
+  handSignal: boolean;
 }
 
 export interface Teacher extends ChannelUser {
@@ -139,6 +140,13 @@ export const useChannel = defineStore('channel', () => {
     socket?.emit('update-webcam', { video: user.video, audio: user.audio });
   }
 
+  function toggleHandSignal(): void {
+    const student = currentUser() as Student;
+
+    student.handSignal = !student.handSignal;
+    socket?.emit('update-handSignal', { handSignal: student.handSignal });
+  }
+
   function stopWebcam(): void {
     const stream = streams[state.clientId];
 
@@ -159,6 +167,10 @@ export const useChannel = defineStore('channel', () => {
     }
 
     return state.students.find((s) => s.id === id);
+  }
+
+  function isStudent(user: ChannelUser): user is Student {
+    return user.id !== state.teacher?.id;
   }
 
   function currentUser(): ChannelUser {
@@ -197,7 +209,12 @@ export const useChannel = defineStore('channel', () => {
       state.clientId = socket?.id || '';
       state.room = room;
       state.students = [];
-      state.teacher = { id: state.clientId, user, video: true, audio: true };
+      state.teacher = {
+        id: state.clientId,
+        user,
+        video: true,
+        audio: true,
+      };
       state.hasName = true;
 
       await router.push({
@@ -358,6 +375,17 @@ export const useChannel = defineStore('channel', () => {
         }
       },
     );
+
+    socket.on(
+      'update-handSignal',
+      (payload: { id: string; handSignal: boolean }) => {
+        const student = userById(payload.id) as Student;
+
+        if (student) {
+          student.handSignal = payload.handSignal;
+        }
+      },
+    );
   }
 
   return {
@@ -369,6 +397,7 @@ export const useChannel = defineStore('channel', () => {
     leaveAsTeacher,
     leave,
     isSelf,
+    isStudent,
     userById,
     currentUser,
     changeName,
@@ -377,6 +406,7 @@ export const useChannel = defineStore('channel', () => {
     getWebcamStream,
     toggleVideo,
     toggleAudio,
+    toggleHandSignal,
     stopWebcam,
   };
 });
